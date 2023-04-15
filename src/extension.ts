7feed7e5b3e4f8ad86d9c720c28314361f2b2c6e
k-yagi <k.yagi914@gmail.com>
2023-04-16 00:14:15 +0900

    minimum implementation

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,81 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
+import * as pluralize from 'pluralize';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	const goToSchemaCommand = vscode.commands.registerCommand('rails-table-schema.goToSchema', async () => {
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			return;
+		}
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "rails-table-schema" is now active!');
+		const document = editor.document;
+		const modelName = getModelNameFromPath(document.fileName);
+		if (!modelName) {
+			return;
+		}
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('rails-table-schema.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Rails Table Schema!');
+		const workspaceFolder = vscode.workspace.getWorkspaceFolder(document.uri);
+		if (!workspaceFolder) {
+			return;
+		}
+
+		const schemaFilePath = path.join(workspaceFolder.uri.fsPath, 'db', 'schema.rb');
+		try {
+			const schemaFileUri = vscode.Uri.file(schemaFilePath);
+			const schemaDoc = await vscode.workspace.openTextDocument(schemaFileUri);
+			const targetEditor = await vscode.window.showTextDocument(schemaDoc);
+
+			const targetLine = findSchemaLine(schemaDoc, modelName);
+			if (targetLine !== null) {
+				const position = new vscode.Position(targetLine, 0);
+				targetEditor.selection = new vscode.Selection(position, position);
+				targetEditor.revealRange(new vscode.Range(position, position));
+			}
+		} catch (error) {
+			vscode.window.showErrorMessage('Could not open schema.rb file.');
+		}
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(goToSchemaCommand);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
+
+function findSchemaLine(document: vscode.TextDocument, modelName: string): number | null {
+	const tableName = getTableNameFromModelName(modelName);
+	const createTableRegex = new RegExp(`create_table "${tableName}"`);
+
+	for (let lineIndex = 0; lineIndex < document.lineCount; lineIndex++) {
+		const line = document.lineAt(lineIndex);
+		if (createTableRegex.test(line.text)) {
+			return lineIndex;
+		}
+	}
+
+	return null;
+}
+
+function getModelNameFromPath(filePath: string): string | null {
+	const relativePath = vscode.workspace.asRelativePath(filePath, false);
+	const match = relativePath.match(/app\/models\/(.+)\.rb/);
+	if (!match) {
+		return null;
+	}
+
+	const namespacePath = match[1];
+	return namespacePath.split('/').map(part => part.charAt(0).toUpperCase() + part.slice(1)).join('::');
+}
+
+function getTableNameFromModelName(modelName: string): string {
+	const parts = modelName.split('::');
+	const lastPart = parts.pop();
+	if (!lastPart) {
+		return '';
+	}
+
+	const pluralizedLastPart = pluralize.plural(lastPart);
+	parts.push(pluralizedLastPart);
+
+	return parts.join('_').toLowerCase();
+}
